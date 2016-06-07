dc261fa24b89e6be5d1774ef2c5eb678181410cc
Petar Nikov <petar.nikov@gmail.com>
2016-06-07 23:44:45 +0300

    homework task start ~homework

---
+++ Cplusplus/week_two/howework/BusinessPark/BusinessPark.cpp
@@ -0,0 +1,13 @@
+//BusinessPark.cpp: Defines the entry point for the console application
+//
+#include <iostream>
+#include <string>
+#include "Building.h"
+
+using namespace std;
+
+int main()
+{
+
+return 0;
+}
