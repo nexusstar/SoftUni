fa40a40bca9e0f321564deba9790cb58e0a57262
Petar Nikov <petar.nikov@gmail.com>
2016-06-25 09:11:33 +0300

    daily task paint

---
+++ Cplusplus/week_four/extra/DailyTask_Paint/paint.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+class Size
+{
+  public:
+    float x;
+    float y;
+};
+
+class PaintObject
+{
+  public:
+    char color[4];
+    Size size;
+    Size sheetSize;
+};
+
+class Whale
+{
+  public:
+    Size size;
+    char sckinColor[4];
+    unsigned short mass;
+};
+
+
+class PaintWhale : public Whale, public PaintObject, public Size
+{
+
+  public:
+    void whaleSizeOnSheet();
+    double scaleFactor();
+};
+
+void PaintWhale::whaleSizeOnSheet()
+{
+  double scaleFactorOnX = this -> Whale::size.x / this -> PaintObject::sheetSize.x;
+  double scaleFactorOnY = this -> Whale::size.y / this -> PaintObject::sheetSize.y;
+
+  double desiredScaleFactor;
+
+  if (scaleFactorOnX > scaleFactorOnY)
+  {
+    desiredScaleFactor = scaleFactorOnX;
+  }
+  else
+  {
+    desiredScaleFactor = scaleFactorOnY;
+  }
+  this -> PaintObject::size.x = this->Whale::size.x / desiredScaleFactor;
+  this -> PaintObject::size.y = this->Whale::size.x / desiredScaleFactor;
+
+}
+
+double PaintWhale::scaleFactor()
+{
+
+  return this -> PaintObject::size.x / this -> Whale::size.x;
+}
+
