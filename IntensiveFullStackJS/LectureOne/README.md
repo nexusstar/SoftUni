187a4853437732bef7ec11569d5670f3ef9d7683
Petar Nikov <petar.nikov@gmail.com>
2016-07-05 18:08:20 +0300

    Clasical Inheritance again with JS

---
+++ IntensiveFullStackJS/LectureOne/README.md
@@ -0,0 +1,6 @@
+# Lecture one themes
+
+## Object-oriented JavaScripti
+
+- primitive types and objects.
+- accessing objects by reference, properties, functions and methods, using this keyword, call, apply,   and bind function methods, prototypal inheritance, polymorphism and method overriding, classes and constructors, classical inheritance and using instanceof, using this, EcmaScript 6 (Harmony, ES 2015) class and constructor syntax, let and var, function lambdas (=>), Promises.
---
+++ IntensiveFullStackJS/LectureOne/class_creation_mdn.js
@@ -0,0 +1,63 @@
+"use strict";
+(function(){
+
+    var Employee = function(){
+        this.name = '';
+        this.dept = 'general';
+    };
+
+    var Manager = function(){
+        Employee.call(this);
+        this.report = [];
+    };
+
+    Manager.prototype = Object.create(Employee.prototype);
+
+    var WorkerBee = function(){
+        Employee.call(this);
+        this.projects = [];
+    };
+
+    WorkerBee.prototype = Object.create(Employee.prototype);
+
+
+    var SalesPerson  = function(){
+        WorkerBee.call(this);
+        this.dept = "sales";
+        this.quota = 100;
+    };
+
+    SalesPerson.prototype = Object.create(WorkerBee.prototype);
+
+    var Engineer = function(){
+        WorkerBee.call(this);
+        this.dept = "engineering";
+        this.machine = "";
+    };
+
+    Engineer.prototype = Object.create(WorkerBee.prototype);
+
+    return {
+        Employee: Employee(),
+        Manager: Manager(),
+        WorkerBee: WorkerBee(),
+        SalesPerson: SalesPerson(),
+        Engineer: Engineer()
+    }
+}());
+
+//new Instance of Engineer
+var jim = new Engineer;
+jim.name = "Jim";
+jim.machine = "Space ship";
+jim.some = "something else";
+
+console.log(Engineer.toString());
+console.log(jim);
+console.log(jim.hasOwnProperty('name'));
+
+
+for (var propName in jim){
+    console.log(propName);
+}
+
