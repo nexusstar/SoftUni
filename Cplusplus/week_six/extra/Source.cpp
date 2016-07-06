c97a0b22c71891b7210ea3357da1f229b284c573
Petar Nikov <petar.nikov@gmail.com>
2016-07-06 18:05:52 +0300

    week_six init

---
+++ Cplusplus/week_six/extra/Source.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <map>
+#include <utility>
+
+class Person
+{
+  public:
+    Person( std::string first, std::string last, int arbitray);
+    ~Person();
+    int getNum();
+    std::string getName();
+  private:
+    std::string firstname;
+    std::string lastname;
+    int arbitrarynumber;
+};
+
+Person::Person(std::string first, std::string last, int arbitrary):
+  firstname(first),lastname(last),arbitrarynumber(arbitrary)
+{}
+Person::~Person()
+{}
+
+int Person::getNum()
+{
+  return arbitrarynumber;
+}
+
+std::string Person::getName()
+{
+  return firstname + " " + lastname;
+}
+
+int main ()
+{
+  Person Kate{"Kate","Gregory", 123};
+  Person Nexus{"Nexus","StarA", 124};
+  Person SomeA{"Nexus", "Star", 125};
+
+  std::map<int, Person> people;
+
+  /* people[Nexus.getNum()] = Nexus; */
+
+  std::pair<int, Person> p (SomeA.getNum(), SomeA);
+  people.insert(p);
+
+  return 0;
+
+}
---
+++ Cplusplus/week_six/extra/map/README.md
@@ -0,0 +1,9 @@
+# map
+
+* set of key-value pairs
+* Grows as needed
+* Kept sorted internally to speed searches
+* Add or access items with []
+
+   - or use pair <> template when adding
+   - can use find() to access also
